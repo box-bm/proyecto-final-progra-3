73b441764f51d5f4736c35b810aa9dbd9b09f983
Brandon Manzo <brandong.manzog@icloud.com>
2024-02-27 00:13:12 -0600

    feat: added utils folder for the project

---
+++ ProyectoFinal/Utils/JWT.cs
@@ -0,0 +1,33 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ProyectoFinal.Utils;
+
+public class JWTConfig(IConfiguration config)
+{
+  private IConfiguration _config = config;
+
+  private string GetKey() => _config["Jwt:Key"] ?? throw new Exception("Jwt:Key not found in appsettings.json");
+  private string GetIssuer() => _config["Jwt:Issuer"] ?? throw new Exception("Jwt:Issuer not found in appsettings.json");
+
+  public string CreateToken(UserClaims userClaims)
+  {
+
+    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GetKey()));
+    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+    var claimProperties = typeof(UserClaims).GetProperties();
+    var claims = new List<Claim>();
+
+    foreach (var porperty in claimProperties)
+    {
+      var value = porperty.GetValue(userClaims);
+      claims.Add(new Claim(porperty.Name, value?.ToString() ?? ""));
+    }
+    var strToken = new JwtSecurityToken(GetIssuer(), GetIssuer(), claims, expires: DateTime.Now.AddDays(1), signingCredentials: credentials);
+
+    return new JwtSecurityTokenHandler().WriteToken(strToken);
+  }
+}
---
+++ ProyectoFinal/Utils/SeedConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProyectoFinal.Entities;
+
+namespace ProyectoFinal.Utils;
+
+
+public class SeedConfig(WebApplication app)
+{
+  public async Task SeedData()
+  {
+    var scopeFactory = app!.Services.GetRequiredService<IServiceScopeFactory>();
+    using var scope = scopeFactory.CreateScope();
+
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+    context.Database.EnsureCreated();
+
+    if (!userManager.Users.Any())
+    {
+      logger.LogInformation("Creando usuario de prueba");
+
+      var newUser = new User
+      {
+        Email = "test@demo.com",
+        UserName = "test.demo",
+        Name = "Test",
+        Address = "Test address",
+      };
+
+      await userManager.CreateAsync(newUser, "P@ss.W0rd");
+      await roleManager.CreateAsync(new IdentityRole
+      {
+        Name = "Admin"
+      });
+
+      await userManager.AddToRoleAsync(newUser, "Admin");
+      await userManager.AddToRoleAsync(newUser, "AnotherRole");
+    }
+  }
+}
