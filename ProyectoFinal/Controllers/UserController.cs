136a3a1c780ecfab539ff37fb00c5841b375ee6a
Brandon Manzo <brandong.manzog@icloud.com>
2024-02-27 00:12:14 -0600

    feat: created first controller

---
+++ ProyectoFinal/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Entities;
+using ProyectoFinal.Models;
+using ProyectoFinal.Utils;
+
+namespace ProyectoFinal;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(AppDbContext context, UserManager<User> userManager, IConfiguration config) : ControllerBase
+{
+  private readonly AppDbContext _context = context;
+  private readonly UserManager<User> userManager = userManager;
+  private IConfiguration _config = config;
+
+  [HttpPost]
+  [Route("register")]
+  public async Task<ActionResult> Create([FromBody] INewUser user)
+  {
+    try
+    {
+      var newUser = new User
+      {
+        UserName = user.UserName,
+        Email = user.Email,
+        Name = user.Name,
+        Address = user.Address
+      };
+
+      var result = await userManager.CreateAsync(newUser, user.Password);
+
+      return Created();
+    }
+    catch (Exception)
+    {
+      return BadRequest();
+    }
+  }
+
+  [HttpPost]
+  [Route("login")]
+  public async Task<ActionResult> Login([FromBody] IUserCredentials userCredentials)
+  {
+    try
+    {
+      var user = await userManager.FindByEmailAsync(userCredentials.Email);
+
+      if (user == null) return NotFound();
+
+      var isValid = await userManager.CheckPasswordAsync(user, userCredentials.Password);
+      if (!isValid) return Unauthorized();
+
+      var roles = await userManager.GetRolesAsync(user);
+
+      var token = new JWTConfig(_config).CreateToken(new UserClaims(user.Name, user.Email!, user.UserName, [.. roles]));
+      return Ok(token);
+    }
+    catch (Exception)
+    {
+      return BadRequest();
+    }
+  }
+
+}
